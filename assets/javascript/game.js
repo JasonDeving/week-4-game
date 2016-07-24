d935db5b314ee2679bfb9012daae3a7a3d354b8a
Admin <admin@Admins-MacBook-Pro.local>
2016-07-24 12:38:44 -0700

    push to github

---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+// variables
+var win = 0;
+var lose = 0;
+var otherNum = [39, 44, 40, 55];
+var chosenNumber = otherNum[Math.floor(Math.random() * otherNum.length)];
+var numbers = [10,11,5,2];
+var randNum = numbers[Math.floor(Math.random() * numbers.length)];
+console.log(randNum)
+var gems = [];
+var images = ["assets/images/gem_1.jpg", "assets/images/gem_2.jpg", "assets/images/gem_3.jpg", "assets/images/gem_4.jpg",]
+var counter = 0;
+$('#winNumber').text(chosenNumber);
+
+for (var i=0; i< numbers.length; i++){
+
+      var imageCrystal = $('<img>');
+      
+      imageCrystal.attr('data-num', numbers[i]);
+
+      imageCrystal.attr('src', images[i]);
+
+      imageCrystal.attr('alt', 'crystals');
+
+      imageCrystal.addClass('crystalImage');
+
+      $('#crystals').append(imageCrystal);
+    }
+    
+    $('.crystalImage').on('click', function(){
+      counter = counter + parseInt($(this).data('num'));
+      
+      $('#score').text(counter);
+
+      if (counter == chosenNumber){
+        alert('You won!!!!');
+        win++;
+		counter = 0;
+		$('#win').text(win);
+		chosenNumber = otherNum[Math.floor(Math.random() * otherNum.length)];
+		console.log(chosenNumber, "win")
+		$('#winNumber').text(chosenNumber);
+      }else if( counter > chosenNumber){
+      	lose++;
+		counter = 0;
+		$('#lose').text(lose);
+		chosenNumber = otherNum[Math.floor(Math.random() * otherNum.length)];
+		
+		$('#winNumber').text(chosenNumber);
+        alert('You lost!');
+      }
+    });
+    $('#winNumber').text(chosenNumber);
