537064e0fe27771b4019d0c20c01bda0e8f2a6ab
Admin <admin@Admins-MacBook-Pro.local>
2016-07-27 18:55:04 -0700

    fixed bug with shuffle with

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,17 @@ var lose = 0;
 var otherNum = [39, 44, 40, 55];
 var chosenNumber = otherNum[Math.floor(Math.random() * otherNum.length)];
 var numbers = [10,11,5,2];
-var numbers = numbers.sort(function(){return 0.5 - Math.random()});
-var images = ["assets/images/gem_1.jpg", "assets/images/gem_2.jpg", "assets/images/gem_3.jpg", "assets/images/gem_4.jpg",]
+    numbers = numbers.sort(function(){return 0.5 - Math.random()});
+var images = ["assets/images/gem_1.jpg", "assets/images/gem_2.jpg", "assets/images/gem_3.jpg", "assets/images/gem_4.jpg"];
 var counter = 0;
 $('#winNumber').text(chosenNumber);
+function shuffle() {
+    numbers = numbers.sort(function(){return 0.5 - Math.random()});
+    images = images.sort(function(){return 0.5 - Math.random()});
+}
 
+function crystalCreator() {
+  shuffle();
   for (var i=0; i< numbers.length; i++){
 
       var imageCrystal = $('<img>');
@@ -20,18 +26,23 @@ $('#winNumber').text(chosenNumber);
       imageCrystal.attr('alt', 'crystals');
 
       imageCrystal.addClass('crystalImage');
-
+  
       $('#crystals').append(imageCrystal);
+    
     } 
-
-function shuffle () {
-  numbers = numbers.sort(function(){return 0.5 - Math.random()});
-  console.log(numbers.length)
-  console.log(numbers)
-  // for (var i=0; i< numbers.length; i++){ 
-  //   $("#crystals img").attr('data-num', numbers[i]);
-  // }
 }
+crystalCreator();
+
+function replace() {
+  shuffle();
+  $(".crystalImage").each(function(i){
+    $(this).attr("data-num", numbers[i]);
+    $(this).attr("src", images[i]);
+  });
+  
+}  
+
+
 
   
     $('.crystalImage').on('click', function(){
@@ -40,23 +51,22 @@ function shuffle () {
       $('#score').text(counter);
 
       if (counter == chosenNumber){
-        alert('You won!!!!');
         win++;
     		counter = 0;
     		$('#win').text(win);
     		chosenNumber = otherNum[Math.floor(Math.random() * otherNum.length)];
-    		$('#winNumber').text(chosenNumber);
-        shuffle();
-        
+        $('#winNumber').text(chosenNumber);
+        replace();
+
       } else if( counter > chosenNumber){
       	lose++;
     		counter = 0;
     		$('#lose').text(lose);
     		chosenNumber = otherNum[Math.floor(Math.random() * otherNum.length)];
     		$('#winNumber').text(chosenNumber);
-        alert('You lost!');
-        shuffle();
+        replace();
       }
     });
       
       $('#winNumber').text(chosenNumber);
+
